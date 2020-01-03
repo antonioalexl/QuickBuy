7d4ad35d2f6547dc3c13b3ec148af8939740ec4d
Alex Lopes <alex.lopes@comlink.com.br>
2020-01-03 17:46:00 -0300

    teste

---
+++ QuickBuy.Dominio/Entidades/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int Id { set; get; }
+        public string Nome { set; get; }
+        public string Descricao { set; get; }
+        public decimal Preco { set; get; }
+    }
+}

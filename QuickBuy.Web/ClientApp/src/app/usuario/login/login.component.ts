0bd065d026f09c5b74fc1c24de0b1857dffac4c2
Alex Lopes <alex.lopes@comlink.com.br>
2020-01-10 16:51:15 -0300

    melhorias formato

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { Usuario } from '../../modelo/usuario';
 
 
 @Component({
-  seletor: "app-login",
+  selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent {
-  
 
+  public usuario;
+  public usuarioAutenticado: boolean;
+
+
+  constructor() { this.usuario = new Usuario();}
+
+
+  public email = "antonio.alex@hotmail.com"
+  public senha = ""
+
+  entrar(): void {
+
+    
+
+
+    alert(this.email);
+
+  }
+  on_Keypress() {
+    alert('foi digitado no campo de email');
+  }
 }

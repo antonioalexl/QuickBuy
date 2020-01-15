6da85b58aacbe391d91c883c0c686987bfb6927f
Alex Lopes <alex.lopes@comlink.com.br>
2020-01-15 17:54:57 -0300

    ver

--- QuickBuy.Web/ClientApp/src/app/servicos/usuario/usuario.servico.ts
+++ QuickBuy.Web/ClientApp/src/app/servicos/usuario/usuario.servico.ts
@@ -3,7 +3,8 @@ import { HttpClient, HttpHeaders } from "@angular/common/http"
 import { Observable } from "rxjs";
 
 @Injectable({
-    
+
+  providedIn: "root"
   
 })
 export class UsuarioServico {

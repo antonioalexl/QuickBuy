4a19b3dc2a4e77bf5244b57143b5e106877c1825
Alex Lopes <alex.lopes@comlink.com.br>
2020-01-20 17:51:24 -0300

    ajustes

--- QuickBuy.Web/ClientApp/src/app/app.module.ts
+++ QuickBuy.Web/ClientApp/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { LoginComponent } from './usuario/login/login.component';
 import { GuardaRotas } from './autorizacao/guarda.rotas';
 import { UsuarioServico } from './servicos/usuario/usuario.servico';
 import { CadastroUsuarioComponent } from './usuario/cadastro/cadastro.usuario.component';
+import { ProdutoServico } from './servicos/produto/produto.servico';
 
 
 @NgModule({
@@ -22,6 +23,7 @@ import { CadastroUsuarioComponent } from './usuario/cadastro/cadastro.usuario.co
     ProdutoComponent,
     LoginComponent,
     CadastroUsuarioComponent
+    
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -34,7 +36,7 @@ import { CadastroUsuarioComponent } from './usuario/cadastro/cadastro.usuario.co
       { path: "novo-usuario", component: CadastroUsuarioComponent }
     ])
   ],
-  providers: [UsuarioServico],
+  providers: [UsuarioServico, ProdutoServico],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
